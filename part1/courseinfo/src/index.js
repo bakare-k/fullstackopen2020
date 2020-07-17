16c280a85a57465bde50429c436e346f8b8fa9f0
@bakare-k <>
2020-07-17 21:20:51 +0100

    1.4, pass objects directly as array to Content and Total

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -17,20 +17,17 @@ const App = () => {
     exercises: 14
   }
 ]
-   
   return (
     <div>
       <Header course = {course} />
-      <Content part1= {parts[0].name} ex1 = {parts[0].exercises} 
-                part2= {parts[1].name} ex2 = {parts[1].exercises}
-                part3= {parts[2].name} ex3 = {parts[2].exercises}/>
-      <Total ex1 = {parts[0].exercises} ex2 = {parts[1].exercises} ex3 = {parts[2].exercises} />
+      <Content parts= {parts}/>
+      <Total parts= {parts}/>
     </div>
   )
 }
 
 const Header = (props) => {
-  console.log(props)
+  //console.log(props)
   return (
     <>
     <h1>{props.course}</h1>
@@ -39,30 +36,30 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
-  console.log(props)
   return (
     <>
-    <Part part = {props.part1} exercise = {props.ex1} />
-    <Part part = {props.part2} exercise = {props.ex2} />
-    <Part part = {props.part3} exercise = {props.ex3} />
+    <Part part = {props.parts[0].name} exercises = {props.parts[0].exercises}/>
+    <Part part = {props.parts[1].name} exercises = {props.parts[1].exercises}/>
+    <Part part = {props.parts[2].name} exercises = {props.parts[2].exercises}/>
     </>
-  )
+    )
 }
 
+
 const Total = (props) => {
-  console.log(props)
+  //console.log(props.parts[0].exercises)
   return (
     <>
-    <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises }</p>
     </>
   )
 }
 
 const Part = (props) => {
-  console.log(props)
+  //console.log(props)
   return (
     <>
-    <p>{props.part} {props.exercise}</p>
+    <p>{props.part} {props.exercises}</p>
     </>
   )
 }
